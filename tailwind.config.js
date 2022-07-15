2c5dc2ed1b367ef15d77747d3b0c229de30bb256
wlowry88 <william.c.lowry@gmail.com>
2022-07-15 10:44:28 -0400

    use a little daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: ["cupcake"]
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
