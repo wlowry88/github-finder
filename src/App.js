817028b4239c76ac8a629973f318e1d927ae576f
wlowry88 <william.c.lowry@gmail.com>
2022-07-15 11:45:59 -0400

    get the 404 home about pages linked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
@@ -7,7 +10,15 @@ function App() {
     <Router>
       <div className ="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>React app to search Github users and see details. Part of the React Front to Back Udemy course by Brad Traversky.</p>
+      <p>Version: 1.0.0</p>
+      <p>Layout By: Hassib Moddasser</p>
+    </div>
+  )
+}
+
+export default About
